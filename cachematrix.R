30e2d1b167dbb201e32a88145b8dd903bb29b18f
jason <jasonhellwig@gmail.com>
2015-11-22 12:53:46 -0800

    Adding changes required for Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions below are used to create a matrix that can hold a
+## reference to it's inverse.  Solving the inverse only needs to be
+## done once to set the reference to the inverse. Afterwards the
+## reference can be used in place of computing the inverse.
 
-## Write a short comment describing this function
+
+## This function will return a list containing methods for
+## getting a matrix and getting and setting it's inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  ## variable holding the inverse of the matrix
+  inv <- NULL
+  
+  ## invserse getters and setters
+  inverse <- function() inv
+  setInverse <- function(newInverse) inv <<- newInverse
+  
+  ## matrix getter
+  get <- function() x
+  
+  ## return value
+  list(get = get, inverse = inverse, setInverse = setInverse)
 }
 
 
-## Write a short comment describing this function
+## This function returns the inverse of a CacheMatrix 
+## and set's the value of the inverse if it is not already set
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  ## Initialize the inverse if it is NULL
+  if(is.null(x$inverse())) {
+    x$setInverse(solve(x$get(), ...))
+  }
+  
+  ## Return a matrix that is the inverse of 'x'
+  x$inverse()
 }
